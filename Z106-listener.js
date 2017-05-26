a4381b97bb484784c54c9b792a4395d8c8ebcabf
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-05-26 11:36:41 +0300

    Adds getDefaultCursor function. Return only unique changes

--- Z106-listener.js
+++ Z106-listener.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const _ = require('lodash');
 const moment = require('moment');
-const debug = require('debug')('Z106-Listener');
+const createDebug = require('debug');
 const utils = require('./utils');
 
 
 function create(base, stashPrefix='stash') {
+  const debug = createDebug(`Z106-Listener-for${base}`);
   const persistedChangesFilename = `.${stashPrefix}_${base}`;
   let alreadyPassedChanges = readPersistedChanges(persistedChangesFilename);
 
@@ -14,7 +15,7 @@ function create(base, stashPrefix='stash') {
     const date = sinceDate.format('YYYYMMDD');
     const time = sinceDate.format('HHmm');
 
-    const result = await connection.execute('select * from FIN01.z106 where Z106_UPDATE_DATE >= :dateVar AND Z106_TIME >= :timeVar ORDER BY Z106_UPDATE_DATE, Z106_TIME ASC', [date, time], {resultSet: true});
+    const result = await connection.execute(`select * from ${base}.z106 where Z106_UPDATE_DATE >= :dateVar AND Z106_TIME >= :timeVar ORDER BY Z106_UPDATE_DATE, Z106_TIME ASC`, [date, time], {resultSet: true});
 
     const rows = await utils.readAllRows(result.resultSet);
 
@@ -29,11 +30,23 @@ function create(base, stashPrefix='stash') {
     alreadyPassedChanges = setOfChangesToPersist;
     await writePersistedChanges(persistedChangesFilename, setOfChangesToPersist);
 
-    return newChanges;
+    return _.uniqWith(newChanges, isEqualChangeObject);
+  }
+
+  async function getDefaultCursor(connection) {
+    debug('Querying default value for the cursor.');
+    const result = await connection.execute(`select * from ${base}.z106 ORDER BY Z106_UPDATE_DATE DESC, Z106_TIME DESC`, [], {resultSet: true});
+    const latestChangeRow = await result.resultSet.getRow();
+    const latestChange = parseZ106Row(latestChangeRow);
+
+    // ensure that the changes from current minute are not returned when cursor is first used.
+    const latestChanges = await getChangesSinceDate(connection, latestChange.date);
+    return _.last(latestChanges).date;
   }
 
   return {
-    getChangesSinceDate
+    getChangesSinceDate,
+    getDefaultCursor
   };
 }
 
